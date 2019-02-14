c213b0fe78cebd315084e714101262ad0af345eb
cherryvenus <1128yeye@sina.com>
2019-02-14 18:17:53 +0800

    first

--- webUserServer/server.js
+++ webUserServer/server.js
@@ -7,40 +7,40 @@ const config = require('./webpack.dev.js');
 const compiler = webpack(config);
 const httpProxy = require('http-proxy');
 
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
-}));
-app.use(require("webpack-hot-middleware")(compiler,{
-  log: false,
-  path: "/__what",
-  heartbeat: 2000
-}));
+// app.use(webpackDevMiddleware(compiler, {
+//   publicPath: config.output.publicPath
+// }));
+// app.use(require("webpack-hot-middleware")(compiler,{
+//   log: false,
+//   path: "/__what",
+//   heartbeat: 2000
+// }));
 
 // //proxy
 // var proxyMiddleWare = require("http-proxy-middleware");
 var proxyPath = "http://www.cherryvenus.com/";
 var proxyOption ={target:proxyPath,changeOrigoin:true};
 // app.use('/slider', proxyMiddleWare(proxyOption))
-// var proxy=httpProxy.createProxyServer({target:'http://www.cherryvenus.com',selfHandleResponse : true})
-const proxy = new httpProxy();
-app.use("/slider/",(req, res, next)=>{
-    proxy.web(req, res, proxyOption, next)
-  //   proxy.on('proxyRes', function (proxyRes, req, res) {
-  //     var body = new Buffer('');
-  //     proxyRes.on('data', function (data) {
-  //         console.log("res from data:", data);
-  //         body = Buffer.concat([body, data]);
-  //     });
-  //     proxyRes.on('end', function () {
-  //         body = Buffer.from(body);
-  //         for(let i in proxyRes.headers){
-  //             res.setHeader(i, proxyRes.headers[i]);
-  //         }
-  //         res.write(body);
-  //         res.end();
-  //         next()
-  //     });
-  // });  
+//var proxy=httpProxy.createProxyServer({target:'http://www.cherryvenus.com',selfHandleResponse : true})
+const proxy = httpProxy.createProxyServer({})
+app.use("/slider/",async (req, res, next)=>{
+    proxy.web(req, res, proxyOption,next)
+    proxy.on('proxyRes', function (proxyRes, req, res) {
+      var body = new Buffer('');
+      proxyRes.on('data', function (data) {
+          body = Buffer.concat([body, data]);
+      });
+      proxyRes.on('end', function () {
+          body = Buffer.from(body);
+          console.log("res from data:", body);
+          for(let i in proxyRes.headers){
+              res.setHeader(i, proxyRes.headers[i]);
+          }
+          res.write("aaa");
+          res.end();
+          
+      });
+  });  
 })
 
 
